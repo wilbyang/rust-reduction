3fb98d1735b9b6e4d67ce1100f8ea8ac2769357d
wilbyang <yang.wilby@gmail.com>
2023-09-03 23:03:10 +0200

    try to use db for storage

--- axum-todo/src/models.rs
+++ axum-todo/src/models.rs
@@ -1,7 +1,11 @@
+use anyhow::Result;
+use libsql_client::client::Client;
+use libsql_client::proto::Stmt;
+use libsql_client::Statement;
+use serde::Serialize;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use uuid::Uuid;
-use serde::Serialize;
 
 pub type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
 
@@ -12,6 +16,51 @@ pub struct Todo {
     pub completed: bool,
 }
 
-struct TodoDb {
-    db: Db,
+pub struct SqliteDb {
+    client: Client,
+}
+
+impl SqliteDb {
+    /// Creates a new database client.
+    /// If the LIBSQL_CLIENT_URL environment variable is not set, a local database will be used.
+    /// It's also possible to use a remote database by setting the LIBSQL_CLIENT_URL environment variable.
+    /// The `mail` table will be automatically created if it does not exist.
+    pub async fn new() -> Result<Self> {
+        if std::env::var("LIBSQL_CLIENT_URL").is_err() {
+            let mut db_path = std::env::temp_dir();
+            db_path.push("todos.db");
+            let db_path = db_path.display();
+            tracing::warn!("LIBSQL_CLIENT_URL not set, using a default local database: {db_path}");
+            std::env::set_var("LIBSQL_CLIENT_URL", format!("file://{db_path}"));
+        }
+        let db = Client::from_env().await?;
+        db.batch(["CREATE TABLE IF NOT EXISTS todo (id text, text text, completed completed)"])
+            .await?;
+        Ok(Self { client: db })
+    }
+    pub async fn get_todos(&self) -> Result<Vec<Todo>> {
+        let mut todos = Vec::new();
+        let rs = self.client.execute("SELECT * FROM todo").await?;
+        for row in &rs.rows {
+            let id: &str = row.try_get(0)?;
+            let text: &str = row.try_get(1)?;
+            let completed: usize = row.try_get(2)?;
+
+            todos.push(Todo {
+                id: Uuid::parse_str(&id)?,
+                text: text.to_string(),
+                completed: completed == 1,
+            });
+        }
+        Ok(todos)
+    }
+    pub async fn save_todo(&self, id: Uuid, text: String, completed: bool) -> Result<()> {
+        self.client
+            .execute(Statement::with_args(
+                "INSERT INTO todo VALUES(?,?,?)",
+                libsql_client::args!(id.to_string(), text, completed as usize),
+            ))
+            .await?;
+        Ok(())
+    }
 }
