4e51008dcf18f718beb739fa1789a653a4eccf2a
wilbyang <yang.wilby@gmail.com>
2023-09-06 22:09:13 +0200

    err handling

--- axum-todo/src/handlers/mod.rs
+++ axum-todo/src/handlers/mod.rs
@@ -2,7 +2,14 @@ mod todo_index;
 mod todo_create;
 mod todo_update;
 mod todo_delete;
+
+use axum::http::StatusCode;
 pub use todo_index::*;
 pub use todo_create::*;
 pub use todo_update::*;
 pub use todo_delete::*;
+fn internal_error(err: anyhow::Error) -> (StatusCode, String)
+
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
--- axum-todo/src/handlers/todo_index.rs
+++ axum-todo/src/handlers/todo_index.rs
@@ -5,8 +5,9 @@ use axum::http::StatusCode;
 use axum::Json;
 use axum::response::IntoResponse;
 use serde::Deserialize;
+use crate::handlers::internal_error;
 
-use crate::models::TodoRepository;
+use crate::models::{Todo, TodoRepository};
 
 // The query parameters for todos index
 #[derive(Debug, Deserialize, Default)]
@@ -18,16 +19,16 @@ pub struct Pagination {
 pub async fn todos_index(
     pagination: Option<Query<Pagination>>,
     State(repo): State<Arc<dyn TodoRepository>>,
-) -> impl IntoResponse {
-    
-
+) -> Result<impl IntoResponse, (StatusCode, String)> {
     let Query(pagination) = pagination.unwrap_or_default();
 
-    match repo.list() {
-        Ok(todos) => (StatusCode::OK, Json("todos")),
-        Err(_) => (StatusCode::OK, Json("todos")),
-    }
-    
+    repo.list().map_err(|e| internal_error(e))
+        .and_then(|todos| {
+            let offset = pagination.offset.unwrap_or(0);
+            let limit = pagination.limit.unwrap_or(100);
+            let todos: Vec<Todo> = todos.into_iter().skip(offset).take(limit).collect();
+            Ok(Json(todos))
+        })
 }
 
 
